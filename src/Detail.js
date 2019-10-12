a70d8060855c19c0663abff999c077e24a0612cb
James Tang <jytang10@gmail.com>
2019-10-12 13:34:01 -0700

    Styled type and date

--- src/Detail.js
+++ src/Detail.js
@@ -1,14 +1,22 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 class Detail extends Component {
   render() {
     const params = this.props.navigation.state.params
     return (
       <View style={styles.container}>
-        <Text>{params.eventName}</Text>
-        <Text>{params.date}</Text>
+        <View style={{height:250, width:'100%', alignSelf:'stretch'}}>
+          <Image resizeMode="contain" source={{uri: params.image}} style={{borderRadius:12, width:'100%', height:'100%', alignSelf:'stretch', flex:1}}></Image>
+        </View>
+        <View>
+          <View style={{flexDirection:'row', marginTop:8, alignItems:'baseline'}}>
+            <Text style={{fontSize:16, fontWeight:'bold'}}>Type</Text>
+            <Text>{params.genre}</Text>
+          </View>
+          <Text style={{fontSize:16, fontWeight:'bold'}}>{params.date}</Text>
+        </View>
       </View>
     );
   }
