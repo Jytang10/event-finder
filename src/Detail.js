a7ba402a028a05515d2e87146f0d6e999290373f
James Tang <jytang10@gmail.com>
2019-10-12 12:29:43 -0700

    Added press navigation functionality and passed data

--- src/Detail.js
+++ src/Detail.js
@@ -2,27 +2,20 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-// create a component
 class Detail extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Detail</Text>
-        <Button title="Go to Map" onPress={() => this.props.navigation.navigate('Map')}></Button>
       </View>
     );
   }
 }
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c6e58',
   },
 });
 
-//make this component available to the app
 export default Detail;
